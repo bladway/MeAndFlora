5dbfb04c810e89a62645a19591900993bfeb98b5
bladway <113307409+bladway@users.noreply.github.com>
2024-04-06 00:20:19 +0300

    minor changes: hub - 1.1-test-2

---
+++ Application/Backend/MainServer/src/main/java/ru/vsu/cs/MeAndFlora/MainServer/controller/AuthorizationController.java
@@ -0,0 +1,18 @@
+package ru.vsu.cs.MeAndFlora.MainServer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping(path = "/auth")
+class AuthorizationController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, this is a server";
+    }
+    
+}
