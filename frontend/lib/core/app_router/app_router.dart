fe7b5735ac26aeca92a3b64d6d6b09cacf2c6796
annusshka <alexandra.wolk@yandex.ru>
2024-04-30 17:37:01 +0300

    update app_router

--- frontend/lib/core/app_router/app_router.dart
+++ frontend/lib/core/app_router/app_router.dart
@@ -1,16 +1,35 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:me_and_flora/core/app_router/history_routes.dart';
+import 'package:me_and_flora/core/app_router/home_routes.dart';
+import 'package:me_and_flora/core/app_router/camera_routes.dart';
+import 'package:me_and_flora/core/app_router/plant_ident_list_routes.dart';
+import 'package:me_and_flora/core/app_router/track_routes.dart';
 
 import '../../feature/account/presentation/account_screen.dart';
+import '../../feature/account_list/presentation/account_list_screen.dart';
+import '../../feature/advertisement/presentation/advertisement_screen.dart';
+import '../../feature/botanic_register/botanic_register_screen.dart';
 import '../../feature/camera/presentation/camera_screen.dart';
+import '../../feature/camera/presentation/camera_wrapper_screen.dart';
 import '../../feature/history/presentation/history_screen.dart';
+import '../../feature/history/presentation/history_wrapper_screen.dart';
 import '../../feature/home/presentation/home_screen.dart';
+import '../../feature/home/presentation/home_wrapper_screen.dart';
 import '../../feature/plant_details/presentation/plant_details_screen.dart';
+import '../../feature/plant_ident_form/presentation/plant_ident_form_screen.dart';
+import '../../feature/plant_public/presentation/plant_public_screen.dart';
 import '../../feature/sign_in/presentation/sign_in_screen.dart';
 import '../../feature/sign_up/presentation/sign_up_screen.dart';
+import '../../feature/statistic/presentation/statistic_screen.dart';
 import '../../feature/track/presentation/track_screen.dart';
+import '../../feature/track/presentation/track_wrapper_screen.dart';
+import '../../feature/unknown_plants/presentation/unknown_plants_screen.dart';
+import '../../feature/unknown_plants/presentation/unknown_plants_wrapper_screen.dart';
 import '../domain/models/models.dart';
+import '../presentation/widgets/navigation/nav_bar_admin/nav_bar_admin_screen.dart';
 import '../presentation/widgets/navigation/nav_bar_auth_user/nav_bar_auth_user_screen.dart';
+import '../presentation/widgets/navigation/nav_bar_botanic/nav_bar_botanic_screen.dart';
 import '../presentation/widgets/navigation/nav_bar_default/nav_bar_default_screen.dart';
 
 part 'app_router.gr.dart';
@@ -23,17 +42,36 @@ class AppRouter extends _$AppRouter {
     AutoRoute(page: PlantDetailsRoute.page),
     AutoRoute(page: SignInRoute.page, path: "/"),
     AutoRoute(page: SignUpRoute.page),
-    AutoRoute(page: NavigationBarDefaultRoute.page, children: [
-      AutoRoute(page: HomeRoute.page),
-      AutoRoute(page: CameraRoute.page),
+    AutoRoute(page: AdvertisementRoute.page),
+    AutoRoute(page: NavBarDefaultRoute.page, children: [
+      HomeRoutes.routes,
+      CameraRoutes.routes,
       AutoRoute(page: AccountRoute.page)
     ]),
     AutoRoute(page: NavBarAuthUserRoute.page, children: [
-      AutoRoute(page: HomeRoute.page),
-      AutoRoute(page: CameraRoute.page),
-      AutoRoute(page: HistoryRoute.page),
-      AutoRoute(page: TrackRoute.page),
+      HomeRoutes.routes,
+      CameraRoutes.routes,
+      HistoryRoutes.routes,
+      TrackRoutes.routes,
       AutoRoute(page: AccountRoute.page)
-    ])
+    ]),
+    AutoRoute(page: NavBarBotanicRoute.page, children: [
+      //AutoRoute(page: HomeRoute.page),
+      //AutoRoute(page: CameraRoute.page),
+      //AutoRoute(page: HistoryRoute.page),
+      //AutoRoute(page: UnknownPlantsRoute.page,),
+      HomeRoutes.routes,
+      CameraRoutes.routes,
+      HistoryRoutes.routes,
+      PlantIdentListRoutes.routes,
+      AutoRoute(page: AccountRoute.page),
+    ]),
+    AutoRoute(page: BotanicRegisterRoute.page),
+    AutoRoute(page: NavBarAdminRoute.page, children: [
+      AutoRoute(page: AccountListRoute.page),
+      AutoRoute(page: PlantPublicRoute.page),
+      AutoRoute(page: StatisticRoute.page),
+      AutoRoute(page: AccountRoute.page)
+    ]),
   ];
 }
--- frontend/lib/core/app_router/app_router.gr.dart
+++ frontend/lib/core/app_router/app_router.gr.dart
@@ -15,14 +15,28 @@ abstract class _$AppRouter extends RootStackRouter {
 
   @override
   final Map<String, PageFactory> pagesMap = {
-    AccountRoute.name: (routeData) {
-      final args = routeData.argsAs<AccountRouteArgs>();
+    AccountListRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: AccountScreen(
-          key: args.key,
-          account: args.account,
-        ),
+        child: const AccountListScreen(),
+      );
+    },
+    AccountRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AccountScreen(),
+      );
+    },
+    AdvertisementRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AdvertisementScreen(),
+      );
+    },
+    BotanicRegisterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const BotanicRegisterScreen(),
       );
     },
     CameraRoute.name: (routeData) {
@@ -31,10 +45,22 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const CameraScreen(),
       );
     },
+    CameraWrapperRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const CameraWrapperScreen()),
+      );
+    },
     HistoryRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const HistoryScreen(),
+      );
+    },
+    HistoryWrapperRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const HistoryWrapperScreen()),
       );
     },
     HomeRoute.name: (routeData) {
@@ -43,16 +69,34 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const HomeScreen(),
       );
     },
+    HomeWrapperRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const HomeWrapperScreen()),
+      );
+    },
+    NavBarAdminRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const NavBarAdminScreen()),
+      );
+    },
     NavBarAuthUserRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const NavBarAuthUserScreen(),
+        child: WrappedRoute(child: const NavBarAuthUserScreen()),
       );
     },
-    NavigationBarDefaultRoute.name: (routeData) {
+    NavBarBotanicRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const NavigationBarDefaultScreen(),
+        child: WrappedRoute(child: const NavBarBotanicScreen()),
+      );
+    },
+    NavBarDefaultRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const NavBarDefaultScreen()),
       );
     },
     PlantDetailsRoute.name: (routeData) {
@@ -62,7 +106,25 @@ abstract class _$AppRouter extends RootStackRouter {
         child: PlantDetailsScreen(
           key: args.key,
           plant: args.plant,
+          lon: args.lon,
+          lat: args.lat,
         ),
+      );
+    },
+    PlantIdentFormRoute.name: (routeData) {
+      final args = routeData.argsAs<PlantIdentFormRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: PlantIdentFormScreen(
+          key: args.key,
+          plant: args.plant,
+        ),
+      );
+    },
+    PlantPublicRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const PlantPublicScreen(),
       );
     },
     SignInRoute.name: (routeData) {
@@ -77,51 +139,93 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const SignUpScreen(),
       );
     },
+    StatisticRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const StatisticScreen(),
+      );
+    },
     TrackRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const TrackScreen(),
       );
     },
+    TrackWrapperRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const TrackWrapperScreen()),
+      );
+    },
+    UnknownPlantsRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const UnknownPlantsScreen(),
+      );
+    },
+    UnknownPlantsWrapperRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: WrappedRoute(child: const UnknownPlantsWrapperScreen()),
+      );
+    },
   };
 }
 
 /// generated route for
+/// [AccountListScreen]
+class AccountListRoute extends PageRouteInfo<void> {
+  const AccountListRoute({List<PageRouteInfo>? children})
+      : super(
+          AccountListRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AccountListRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
 /// [AccountScreen]
-class AccountRoute extends PageRouteInfo<AccountRouteArgs> {
-  AccountRoute({
-    Key? key,
-    required Account account,
-    List<PageRouteInfo>? children,
-  }) : super(
+class AccountRoute extends PageRouteInfo<void> {
+  const AccountRoute({List<PageRouteInfo>? children})
+      : super(
           AccountRoute.name,
-          args: AccountRouteArgs(
-            key: key,
-            account: account,
-          ),
           initialChildren: children,
         );
 
   static const String name = 'AccountRoute';
 
-  static const PageInfo<AccountRouteArgs> page =
-      PageInfo<AccountRouteArgs>(name);
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
-class AccountRouteArgs {
-  const AccountRouteArgs({
-    this.key,
-    required this.account,
-  });
+/// generated route for
+/// [AdvertisementScreen]
+class AdvertisementRoute extends PageRouteInfo<void> {
+  const AdvertisementRoute({List<PageRouteInfo>? children})
+      : super(
+          AdvertisementRoute.name,
+          initialChildren: children,
+        );
 
-  final Key? key;
+  static const String name = 'AdvertisementRoute';
 
-  final Account account;
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
 
-  @override
-  String toString() {
-    return 'AccountRouteArgs{key: $key, account: $account}';
-  }
+/// generated route for
+/// [BotanicRegisterScreen]
+class BotanicRegisterRoute extends PageRouteInfo<void> {
+  const BotanicRegisterRoute({List<PageRouteInfo>? children})
+      : super(
+          BotanicRegisterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'BotanicRegisterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
 /// generated route for
@@ -134,6 +238,20 @@ class CameraRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'CameraRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CameraWrapperScreen]
+class CameraWrapperRoute extends PageRouteInfo<void> {
+  const CameraWrapperRoute({List<PageRouteInfo>? children})
+      : super(
+          CameraWrapperRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CameraWrapperRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -153,6 +271,20 @@ class HistoryRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
+/// [HistoryWrapperScreen]
+class HistoryWrapperRoute extends PageRouteInfo<void> {
+  const HistoryWrapperRoute({List<PageRouteInfo>? children})
+      : super(
+          HistoryWrapperRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HistoryWrapperRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
 /// [HomeScreen]
 class HomeRoute extends PageRouteInfo<void> {
   const HomeRoute({List<PageRouteInfo>? children})
@@ -162,6 +294,34 @@ class HomeRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [HomeWrapperScreen]
+class HomeWrapperRoute extends PageRouteInfo<void> {
+  const HomeWrapperRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeWrapperRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeWrapperRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [NavBarAdminScreen]
+class NavBarAdminRoute extends PageRouteInfo<void> {
+  const NavBarAdminRoute({List<PageRouteInfo>? children})
+      : super(
+          NavBarAdminRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'NavBarAdminRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -181,15 +341,29 @@ class NavBarAuthUserRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [NavigationBarDefaultScreen]
-class NavigationBarDefaultRoute extends PageRouteInfo<void> {
-  const NavigationBarDefaultRoute({List<PageRouteInfo>? children})
+/// [NavBarBotanicScreen]
+class NavBarBotanicRoute extends PageRouteInfo<void> {
+  const NavBarBotanicRoute({List<PageRouteInfo>? children})
       : super(
-          NavigationBarDefaultRoute.name,
+          NavBarBotanicRoute.name,
           initialChildren: children,
         );
 
-  static const String name = 'NavigationBarDefaultRoute';
+  static const String name = 'NavBarBotanicRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [NavBarDefaultScreen]
+class NavBarDefaultRoute extends PageRouteInfo<void> {
+  const NavBarDefaultRoute({List<PageRouteInfo>? children})
+      : super(
+          NavBarDefaultRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'NavBarDefaultRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -200,12 +374,16 @@ class PlantDetailsRoute extends PageRouteInfo<PlantDetailsRouteArgs> {
   PlantDetailsRoute({
     Key? key,
     required Plant plant,
+    double? lon,
+    double? lat,
     List<PageRouteInfo>? children,
   }) : super(
           PlantDetailsRoute.name,
           args: PlantDetailsRouteArgs(
             key: key,
             plant: plant,
+            lon: lon,
+            lat: lat,
           ),
           initialChildren: children,
         );
@@ -220,6 +398,50 @@ class PlantDetailsRouteArgs {
   const PlantDetailsRouteArgs({
     this.key,
     required this.plant,
+    this.lon,
+    this.lat,
+  });
+
+  final Key? key;
+
+  final Plant plant;
+
+  final double? lon;
+
+  final double? lat;
+
+  @override
+  String toString() {
+    return 'PlantDetailsRouteArgs{key: $key, plant: $plant, lon: $lon, lat: $lat}';
+  }
+}
+
+/// generated route for
+/// [PlantIdentFormScreen]
+class PlantIdentFormRoute extends PageRouteInfo<PlantIdentFormRouteArgs> {
+  PlantIdentFormRoute({
+    Key? key,
+    required Plant plant,
+    List<PageRouteInfo>? children,
+  }) : super(
+          PlantIdentFormRoute.name,
+          args: PlantIdentFormRouteArgs(
+            key: key,
+            plant: plant,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'PlantIdentFormRoute';
+
+  static const PageInfo<PlantIdentFormRouteArgs> page =
+      PageInfo<PlantIdentFormRouteArgs>(name);
+}
+
+class PlantIdentFormRouteArgs {
+  const PlantIdentFormRouteArgs({
+    this.key,
+    required this.plant,
   });
 
   final Key? key;
@@ -228,8 +450,22 @@ class PlantDetailsRouteArgs {
 
   @override
   String toString() {
-    return 'PlantDetailsRouteArgs{key: $key, plant: $plant}';
+    return 'PlantIdentFormRouteArgs{key: $key, plant: $plant}';
   }
+}
+
+/// generated route for
+/// [PlantPublicScreen]
+class PlantPublicRoute extends PageRouteInfo<void> {
+  const PlantPublicRoute({List<PageRouteInfo>? children})
+      : super(
+          PlantPublicRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'PlantPublicRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
 }
 
 /// generated route for
@@ -261,6 +497,20 @@ class SignUpRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
+/// [StatisticScreen]
+class StatisticRoute extends PageRouteInfo<void> {
+  const StatisticRoute({List<PageRouteInfo>? children})
+      : super(
+          StatisticRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'StatisticRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
 /// [TrackScreen]
 class TrackRoute extends PageRouteInfo<void> {
   const TrackRoute({List<PageRouteInfo>? children})
@@ -270,6 +520,48 @@ class TrackRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'TrackRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [TrackWrapperScreen]
+class TrackWrapperRoute extends PageRouteInfo<void> {
+  const TrackWrapperRoute({List<PageRouteInfo>? children})
+      : super(
+          TrackWrapperRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'TrackWrapperRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [UnknownPlantsScreen]
+class UnknownPlantsRoute extends PageRouteInfo<void> {
+  const UnknownPlantsRoute({List<PageRouteInfo>? children})
+      : super(
+          UnknownPlantsRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'UnknownPlantsRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [UnknownPlantsWrapperScreen]
+class UnknownPlantsWrapperRoute extends PageRouteInfo<void> {
+  const UnknownPlantsWrapperRoute({List<PageRouteInfo>? children})
+      : super(
+          UnknownPlantsWrapperRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'UnknownPlantsWrapperRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
