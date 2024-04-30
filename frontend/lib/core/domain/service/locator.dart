1991e962a255e36efea7ac406ced6c9edcad0857
annusshka <alexandra.wolk@yandex.ru>
2024-04-30 17:38:13 +0300

    update services

--- frontend/lib/core/domain/service/locator.dart
+++ frontend/lib/core/domain/service/locator.dart
@@ -1,11 +1,21 @@
 import 'package:get_it/get_it.dart';
-import 'package:me_and_flora/core/domain/service/history_service.dart';
 
+import 'account_service.dart';
+import 'auth_service.dart';
 import 'camera_service.dart';
+import 'history_service.dart';
+import 'plant_service.dart';
+import 'statistic_service.dart';
+import 'track_service.dart';
 
 final locator = GetIt.instance;
 
 void setupLocator() {
+  locator.registerLazySingleton<AccountService>(() => AccountService());
+  locator.registerLazySingleton<AuthService>(() => AuthService());
   locator.registerLazySingleton<CameraService>(() => CameraService());
   locator.registerLazySingleton<HistoryService>(() => HistoryService());
+  locator.registerLazySingleton<PlantService>(() => PlantService());
+  locator.registerLazySingleton<StatisticService>(() => StatisticService());
+  locator.registerLazySingleton<TrackService>(() => TrackService());
 }
